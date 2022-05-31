03859363c0cdab1372bb4fcc687b6e80f4e45e56
llkeji <61413853+llkeji@users.noreply.github.com>
2022-05-31 11:46:08 +0800

    Delete list.php

--- list.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$arr = Array(
-  Array('name'=>'张三','age'=>18),
-  Array('name'=>'李四','age'=>18),
-  Array('name'=>'王五','age'=>18),
-  Array('name'=>'法外狂徒','age'=>18),
-);
-echo json_encode($arr);
-?>
